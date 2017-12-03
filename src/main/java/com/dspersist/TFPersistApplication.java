18f9bb5904612b14fad60d92c26941a8d5471993
Gabriel Cesar <gcal1@live.com>
2017-12-03 03:35:45 -0300

    connection pooled

--- src/main/java/com/dspersist/TFPersistApplication.java
+++ src/main/java/com/dspersist/TFPersistApplication.java
@@ -1,9 +1,13 @@
 package com.dspersist;
 
+import com.dspersist.configurations.EmbeddedTomcatConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({EmbeddedTomcatConfiguration.class})
 public class TFPersistApplication{
 
 	public static void main(String[] args) {
